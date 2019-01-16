bbad5a7a13c6c08fca3293730714d36e61898ca0
阿小庆 <1756260160@qq.com>
2019-01-16 23:00:38 +0800

    乱码

--- 01read_show.py
+++ 01read_show.py
@@ -1,7 +1,8 @@
+# encoding:utf-8
 import cv2
 
 img = cv2.imread("./cat.jpg")
-cv2.namedWindow("Image")
-cv2.imshow("Image", img)
+cv2.namedWindow("Image显示")
+cv2.imshow("Image显示", img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
